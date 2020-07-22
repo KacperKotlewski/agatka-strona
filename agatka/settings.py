5682c64b19f60695a06e17e0f09f7915a700a997
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-07-22 21:15:57 +0200

    dodano appke 'pages' gdzie przeniosłem baze danych z homepage

--- agatka/settings.py
+++ agatka/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'pages',
     'homepage',
     'about_me',
     'contact',
--- agatka/useful.py
+++ agatka/useful.py
@@ -1,4 +1,4 @@
-from homepage import models as models_homepage
+from pages import models as page_models
 
 from django.shortcuts import render, redirect, HttpResponse
 from django.http import JsonResponse
@@ -7,14 +7,14 @@ from django.contrib import messages
 
 def base_context(request):
     isDark = request.COOKIES['isDark'] if ('isDark' in request.COOKIES) else False
-    menu_tabs = models_homepage.MenuTab.objects.filter(is_a_logo = False, display = True).order_by('-priority')
+    menu_tabs = page_models.MenuTab.objects.filter(is_a_logo = False, display = True).order_by('-priority')
     for tab in menu_tabs:
         try:
             tab.link = tab.page.friendly_link if not tab.is_external_link else tab.external_link
         except:
             tab.link = None
     
-    Logo = models_homepage.MenuTab.objects.filter(is_a_logo = True, display = True)[0]
+    Logo = page_models.MenuTab.objects.filter(is_a_logo = True, display = True)[0]
     ctx = {
         "title" : "Agatka",
         "show_title" : False,
@@ -30,9 +30,9 @@ def base_context(request):
 def set_navname(name, ctx):
     ctx["id_name"] = name
     try:
-        page = models_homepage.Page.objects.filter(id_name=name)[0]
+        page = page_models.Page.objects.filter(id_name=name)[0]
         ctx["page"] = page
-        ctx["settings"] = models_homepage.PagesSetting.objects.filter(page=page)[0]
+        ctx["settings"] = page_models.PagesSetting.objects.filter(page=page)[0]
         ctx["title"] = page.title
         ctx["show_title"] = True
     except:
