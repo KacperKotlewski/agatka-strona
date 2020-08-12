8a37ead9d946a2f7523923131b8dcf4eafbc3968
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-08-12 20:03:17 +0200

    quick debug of error with image saveing v2

--- images/models.py
+++ images/models.py
@@ -90,8 +90,8 @@ def auto_delete_file_on_change(sender, instance, **kwargs):
             new_file = instance.background_image
     try:
         if not (not old_file):
-        if not old_file == new_file:
-            if os.path.isfile(old_file.path):
-                os.remove(old_file.path)
+            if not old_file == new_file:
+                if os.path.isfile(old_file.path):
+                    os.remove(old_file.path)
     except:
         pass
