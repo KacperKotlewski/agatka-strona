fff1a42c383c08c9bdce2e5413cca75e7de48e88
KacperKotlewski <kotlewski.kacper@gmail.com>
2020-08-22 21:22:24 +0200

    deleted old temps

--- agatka/static_dirs/static/js/scrollTo.js
+++
@@ -1,7 +0,0 @@
-function scrollToElem(whereToScroll) {
-    
-    var n = $(whereToScroll).offset().top;
-    var m = $('#menuback').offset().top +$('#menuback').height();
-    
-    $('html, body').animate({ scrollTop: n-m+1 }, 1500);
-}
